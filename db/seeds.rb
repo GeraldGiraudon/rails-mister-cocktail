5ce35ac7bc642fa712b1ed78db736201d11cac5e
gerald giraudon <gerald13006@gmail.com>
2019-11-21 17:14:09 +0100

    first view ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+i = Ingredient.create(name: "mint leaves")
+
+c = Cocktail.create(name: "Wisky coca")
+
+d = Dose.create(description: "5cl", cocktail_id: c.id, ingredient_id: i.id)
